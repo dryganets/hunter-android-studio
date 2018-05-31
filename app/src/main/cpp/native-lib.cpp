7df6a08df48cd472dd6bd84dadb173757ec126e7
Sergei Dryganets <sergeyd@microsoft.com>
2018-05-31 11:30:03 -0700

    bug fixes. First completely working version

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -11,5 +11,5 @@ Java_com_dryganets_hunter_MainActivity_stringFromJNI(
     JNIEnv *env,
     jobject /* this */) {
   std::string hello = "Hello from C++";
-  return env->NewStringUTF(to_upper_copy(hello).c_str());
+  return env->NewStringUTF(boost::to_upper_copy(hello).c_str());
 }
